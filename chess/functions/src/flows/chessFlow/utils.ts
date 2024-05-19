e00fdb310e935b22e2f5ad976b6117500f92fab4
Jacob Cable <jacobcable94@gmail.com>
2024-05-19 12:09:03 -0700

    feat: add intro comment on reset

--- chess/functions/src/flows/chessFlow/utils.ts
+++ chess/functions/src/flows/chessFlow/utils.ts
@@ -13,7 +13,8 @@ export const resetGame = (
   return {
     moveInPGNNotation: "",
     reasoning: "Game reset",
-    smarmyComment: "",
+    smarmyComment:
+      "Hello! I am Gemini and I play chess. Make your move if you dare!",
     availableMoves: game.moves(),
     gameHistory: [],
     position: game.fen(),
